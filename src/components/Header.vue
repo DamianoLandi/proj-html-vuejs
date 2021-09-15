60d5898b8452d0d16de4288ede6709ce9896244e
DamianoLandi <damianolandi01@gmail.com>
2021-09-15 17:01:00 +0200

    Structure

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>ciao</h2>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>ciaoo</h2>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+}
+</script>
+
+<style>
+
+</style>
